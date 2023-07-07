a157dcf289471cd85c987017d6ca3773c86af3ac
JeffBeckLim <101444919+JeffBeckLim@users.noreply.github.com>
2023-07-08 00:03:00 +0800

    Assets and styles

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin-ledger', function (){
+    return view('admin-components.admin-layout');
+});
