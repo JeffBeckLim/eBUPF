2a1335fca733d4c9803a5d79d21a02207e7edb13
sagitace <aaronlabini27.official@gmail.com>
2023-09-23 20:30:02 +0800

    Member Calculator

--- resources/views/member-views/calculator/calculator.blade.php
+++ resources/views/member-views/calculator/calculator.blade.php
@@ -3,22 +3,21 @@
 @section('content')
 
     <div style="border-radius: 20px; background: #F4F4F4; margin-top: 15px;">
-        <div class="d-flex align-items-center p-2 gap-3">
+        <div class="d-flex align-items-center p-3 gap-3">
             <img src="{{asset('icons/calculator.svg')}}" alt="calculator logo" width="30px" height="50px">
             <span style="font-size: 20px; font-weight: bold; color: #000834;">Loan Calculator</span>
         </div>
-        <div style="margin-left: 100px; margin-top: -10px; font-size: 0.9rem;">
+        <div style="margin-left: 100px; margin-top: -15px; font-size: 0.9rem;">
            proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit
         </div>
 
         <div class="row gap-md-5 mx-4 mt-3">
             <div class="col-md-4 bg-white rounded-3">
-                {{-- Input for Loan Category and Loan amount and loan term --}}
                 <form action="#">
-                    <div class="p-3">
+                    <div class="p-4">
                         <span class="fw-bold fs-6">Loan Category</span> <br>
                         <div style="padding: 5px 0 0 13px;">
-                            <div class="row">
+                            <div class="row pt-1">
                                 <div class="col-lg-6">
                                     <input type="radio" id="mpl" name="loan_category" value="mpl">
                                     <label for="mpl" class="fs-7">Multi-Purpose</label>
@@ -29,11 +28,126 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="row mt-2 pt-2">
+                            <div class="col-md-9">
+                                <label for="amount" class="fw-bold">Amount</label>
+                                <div class="d-flex align-items-center">
+                                    <input type="number" name="amount" id="amount" class="form-control" placeholder="Enter Amount">
+                                    <span style="margin-left: 5px;">
+                                        <i class="bi bi-info-circle" data-bs-toggle="tooltip" data-bs-placement="top" title="Additional information about the amount"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <label for="term" class="fw-bold">Year/s</label>
+                                <select name="term" id="term" class="form-control">
+                                    <option value="" disabled selected>0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="mt-2 pt-2">
+                            <label for="prevLoan" class="fw-bold fs-6">Add Previous Loan</label>
+                            <input type="number" name="prevLoan" id="prevLoan" class="form-control" placeholder="Enter previous loan">
+                        </div>
+                        <div class="d-flex justify-content-end mt-3 pt-2">
+                            <button type="reset" class="btn btn-outline-secondary me-2 rounded-2 bg-secondary text-white">Clear</button>
+                            <button type="submit" class="btn btn-primary rounded-2 text-white" style="background: #FF6F19; border: none;">Calculate</button>
+                        </div>
                     </div>
                 </form>
             </div>
             <div class="col-md-7 bg-white rounded-3">
-
+                <div class="p-4">
+                    <div class="fw-bold fs-6">
+                        Result
+                    </div>
+                    <div class="row">
+                        <div class="col-md-7">
+                            <div class="row mt-2">
+                                <div class="col-7 fs-7">
+                                    Monthly principal
+                                </div>
+                                <div class="col-5 fs-7 fw-bold">
+                                    0.00
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-7 fs-7">
+                                    Monthly Interest
+                                </div>
+                                <div class="col-5 fs-7 fw-bold">
+                                    0.00
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-7 fs-7">
+                                    Monthly Payable
+                                </div>
+                                <div class="col-5 fs-7 fw-bold">
+                                    0.00
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-7 fs-7">
+                                    Total Interest
+                                </div>
+                                <div class="col-5 fs-7 fw-bold">
+                                    0.00
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-7 fs-7">
+                                    Total Amount
+                                </div>
+                                <div class="col-5 fs-7 fw-bold">
+                                    0.00
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-7 fs-7">
+                                    Total Months
+                                </div>
+                                <div class="col-5 fs-7 fw-bold">
+                                    0.00
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            Graph
+                        </div>
+                        <div>
+                            <a href="" class="btn d-flex justify-content-center align-items-center fs-7 text-white"><span style="border-radius: 10px;background: #0092D1; padding: 8px 15px;">View Amortization Table</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="p-4">
+            <div class="fw-bold fs-6">
+                Loan Categories
+            </div>
+            <div class="fs-7 mt-2" style="margin-left: 25px;">
+                <span class="fw-bold">Multi-Purpose Loan </span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut.
+            </div>
+            <div class="fs-7 mt-2" style="margin-left: 25px;">
+                <span class="fw-bold">Housing Loan </span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut.
+            </div>
+            <div class="fs-7 mt-2">
+                <span class="fw-bold">Monthly Principal and Monthly Interest</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut.
+            </div>
+            <div class="fs-7 mt-2">
+                <span class="fw-bold">Monthly Payable</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut.
+            </div>
+            <div class="fs-7 mt-2">
+                <span class="fw-bold">Total Amount</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut.
+            </div>
+            <div class="fs-7 mt-2">
+                <span class="fw-bold">Total Months</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut.
             </div>
         </div>
     </div>
