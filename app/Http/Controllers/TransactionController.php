640eeba7fe0cca62e6ef22438b8a41cc79c3af3f
sagitace <aaronlabini27.official@gmail.com>
2023-09-23 22:10:58 +0800

    Member Transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\Payment;
+use App\Models\Loan;
+
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     public function show(){
-        return view('member-views.transactions.member-transactions');
+        $transactionPayments = Payment::where('member_id', Auth::user()->member->id)->get();
+        $transactionLoans = Loan::where('member_id', Auth::user()->member->id)->get();
+        //combine transactions
+        $unsortedTransactions = $transactionLoans->concat($transactionPayments);
+        //sort transactions by date
+        $transactions = $unsortedTransactions->sortByDesc('created_at');
+        //check if there are transactions
+        return view('member-views.transactions.member-transactions', [
+            'transactions' => $transactions,
+        ]);
     }
 }
