269264a81fa5bead250d650d75700eae6a2516cf
sagitace <aaronlabini27.official@gmail.com>
2023-10-20 02:33:14 +0800

    Receivables - Yearly Summary

--- resources/views/admin-views/admin-receivables/admin-receivables.blade.php
+++ resources/views/admin-views/admin-receivables/admin-receivables.blade.php
@@ -25,7 +25,7 @@
                     <div class="col-6">
                         <a class="btn border rounded-end-0 w-100 h-100 bg-white
                         {{$loan_type == 'mpl' ? 'fw-bold shadow-sm scale-1-active' : ''}}
-                        " href="{{route('admin.receivables', 'mpl')}}">
+                        " href="{{route('admin.receivables', ['report' => 'quarterly', 'loan_type' => 'mpl'])}}">
                         <img src="{{asset('icons/MPL-mini.svg')}}" alt="" style="width: 20px;">
                         Multi-Purpose Loan Receivables</a>
                     </div>
@@ -33,7 +33,7 @@
                         <a
                         class="btn border rounded-start-0 w-100 h-100 bg-white
                         {{$loan_type == 'hsl' ? 'fw-bold shadow-sm scale-1-active' : ''}}"
-                        href="{{route('admin.receivables', 'hsl')}}">
+                        href="{{route('admin.receivables', ['report' => 'quarterly', 'loan_type' => 'hsl'])}}">
                         <img src="{{asset('icons/HSL-mini.svg')}}" alt="" style="width: 20px;">
                         Housing Loan Receivables
                     </a>
@@ -42,7 +42,7 @@
 
                 <div class="row">
                     <div class="col-md-6">
-                        <form method="get" action="{{ route('admin.receivables', ['loan_type' => $loan_type]) }}">
+                        <form method="get" action="{{ route('admin.receivables', ['report' => $report, 'loan_type' => $loan_type]) }}">
                             <div class="filter-group gap-3 mt-4">
                                 <div class="form-group fg-admin" style="width: 150px; position: relative;">
                                     <select name="unitSelect" class="form-control bg-white border-0">
@@ -75,9 +75,18 @@
                     </div>
 
                     <div class="col-md-6 text-end mt-4">
-                        <a href="{{route('admin.receivables', 'mpl')}}" class="btn fs-6"><img width="24" height="24" src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/external-quarter-date-and-time-tanah-basah-basic-outline-tanah-basah-2.png" alt="external-quarter-date-and-time-tanah-basah-basic-outline-tanah-basah-2"/> Quarterly</a>
-                        <a href="{{route('admin.receivables.summary')}}" class="btn fs-6"><i class="bi bi-calendar"></i> Yearly Summary</a>
-                        <a href="{{route('admin.receivables.remit')}}" class="btn fs-6"><img width="18" height="20" src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/24/external-bar-chart-in-down-trend-after-market-crash-business-regular-tal-revivo.png" alt="external-bar-chart-in-down-trend-after-market-crash-business-regular-tal-revivo"/> Yearly Remit</a>
+                        <a href="{{ route('admin.receivables', ['report' => 'quarterly', 'loan_type' => 'mpl']) }}" class="btn fs-6 {{$report == 'quarterly' ? 'fw-bold' : ''}}">
+                            <img width="24" height="24" src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/external-quarter-date-and-time-tanah-basah-basic-outline-tanah-basah-2.png" alt="quarterly logo"/>
+                            Quarterly
+                        </a>
+                        <a href="{{ route('admin.receivables.summary', ['report' => 'summary', 'loan_type' => 'mpl']) }}" class="btn fs-6 {{ $report == 'summary' ? 'fw-bold' : '' }}">
+                            <i class="bi bi-calendar"></i>
+                            Yearly Summary
+                        </a>
+                        <a href="{{ route('admin.receivables.remit', ['report' => 'remit', 'loan_type' => 'mpl']) }}" class="btn fs-6 {{ $report == 'remit' ? 'fw-bold' : '' }}">
+                            <img width="18" height="20" src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/24/external-bar-chart-in-down-trend-after-market-crash-business-regular-tal-revivo.png" alt="remit logo"/>
+                            Yearly Remit
+                        </a>
                     </div>
                 </div>
 
