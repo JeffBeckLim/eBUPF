421ea251050ff44c88fc5ab5275b3b4162c67f7d
JeffBeckLim <101444919+JeffBeckLim@users.noreply.github.com>
2023-10-31 20:40:35 +0800

    add member

--- app/Http/Controllers/AdminCreateMemberController.php
+++ app/Http/Controllers/AdminCreateMemberController.php
@@ -2,15 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\User;
+use App\Models\Member;
+use App\Rules\EmailDomain;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AdminCreateMemberController extends Controller
 {
     public function show(){
-        return view('admin-views.admin-create-member.admin-create-member');
+        $units = Unit::all();
+        return view('admin-views.admin-create-member.admin-create-member', compact('units'));
     }
     public function create(Request $request){
-       dd($request); 
+        $formFields = $request->validate([
+            'unit_id'=> 'required', 
+            'firstname'=> 'required',
+            'lastname'=> 'required',
+
+            'email' => ['required', 'string', 'email:rfc,dns', 'max:255', 'unique:users', new EmailDomain('bicol-u.edu.ph')],
+            'password' => ['required', 'string', 'confirmed', 'min:8', 'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d\s])[A-Za-z\d\S]+$/'],
+            
+            'middlename'=> 'nullable',
+            
+            'contact_num'=> 'required',
+
+            'address'=> 'required',
+            'date_of_birth'=> 'required',
+            'tin_num'=> 'required',
+            'position'=> 'nullable',
+
+            'employee_num'=> 'nullable',
+            'bu_appointment_date'=> 'nullable',
+            'sex'=> 'required',
+            'monthly_salary'=> 'required',
+            'appointment_status'=> 'required',
+        ]);
+
+        $user=User::create([
+            'email' => $formFields['email'],
+            'password' => Hash::make($formFields['password']),
+            'user_type' => 'member',
+        ]);
+
+        $member = Member::create([
+            'user_id'=>$user->id,
+             
+            'firstname' => $formFields['firstname'],
+            'lastname' => $formFields['lastname'],
+            'middlename'=> $formFields['middlename'],
+            
+            'contact_num'=> '+63'.$formFields['contact_num'],
+            'unit_id'=> $formFields['unit_id'],
+
+            'address'=> $formFields['address'],
+            'date_of_birth'=> $formFields['date_of_birth'],
+            'tin_num'=> $formFields['tin_num'],
+            'position'=> $formFields['position'],
+
+            'employee_num'=> $formFields['employee_num'],
+            'bu_appointment_date'=> $formFields['bu_appointment_date'],
+            'sex'=> $formFields['sex'],
+            'monthly_salary'=> $formFields['monthly_salary'],
+            'appointment_status'=> $formFields['appointment_status'],
+
+            'agree_to_terms' =>1,
+            'agree_to_certify'=>1,
+            'agree_to_authorize'=>1,
+        ]);
+
+        if($user != null && $member != null){
+            return back()->with('passed','Account Created!');
+        }
+        
+
     }
     
 }
--- resources/views/admin-views/admin-create-member/admin-create-member.blade.php
+++ resources/views/admin-views/admin-create-member/admin-create-member.blade.php
@@ -6,6 +6,16 @@
     <div class="row mt-2">
         <div class="container">
             <div class="row border  mx-4 bg-white rounded p-4">
+                
+                @if (session('passed'))
+                <div class="col-12">
+                    <div class="alert alert-dismissible fade show" role="alert" style="background-color: #e8cfff; border: 1px solid #994ede">
+                        {{session('passed')}}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>    
+                @endif
+                
                 <div class="col-12">
                     <h5 class="fw-bold p-3"><i style="color: #994ede" class="bi bi-person-plus-fill"></i> Create Member Account</h5>
 
@@ -20,38 +30,66 @@
 
 
                                 <div class="col-6 ">
-                                    <label for="firstname">First name</label>
-                                    <input class="form-control" type="text" name="firstname" id="firstname">
+                                    <label for="firstname">First name <span class="text-danger fw-bold">*</span></label>
+                                    <input class="form-control" type="text" name="firstname" id="firstname" value="{{old('firstname')}}">
+                                    @error('firstname')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-6">
-                                    <label for="lastname">Last name</label>
-                                    <input class="form-control" type="text" name="" id="">
+                                    <label for="lastname">Last name <span class="text-danger fw-bold">*</span></label>
+                                    <input class="form-control" type="text" name="lastname" id="lastname" value="{{old('lastname')}}" >
+                                    @error('lastname')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
 
 
                                 <div class="col-6 ">
-                                    <label for="middlename">Midle Name</label>
-                                    <input class="form-control" type="text" name="middlename" id="middlename">
+                                    <label for="middlename">Midle Name <span class="text-danger fw-bold">*</span></label>
+                                    <input class="form-control" type="text" name="middlename" id="middlename" value="{{old('middlename')}}" >
+                                    @error('middlename')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-6">
-                                    <label for="date_of_birth">Birth date</label>
-                                    <input class="form-control" type="date" name="date_of_birth" id="date_of_birth">
+                                    <label for="date_of_birth">Birth date <span class="text-danger fw-bold">*</span></label>
+                                    <input class="form-control" type="date" name="date_of_birth" id="date_of_birth"  value="{{old('date_of_birth')}}">
+                                    @error('date_of_birth')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
 
 
                                 <div class="col-6 ">
-                                    <label for="middlename">Sex</label></label>
-                                    <input class="form-control" type="text" name="sex" id="sex">
+                                    <label for="middlename">Sex <span class="text-danger fw-bold">*</span></label>
+                                    <select class="form-select form-control" aria-label="Default select example" name="sex">
+                                        <option value="male" {{ old('sex') == 'male'? 'selected' : '' }}>male</option>
+                                        <option value="female" {{ old('sex') == 'female'? 'selected' : '' }}>female</option>
+                                    </select>
+                                      @error('sex')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-6 ">
-                                    <label for="contact_num">Contact Number</label></label>
-                                    <input class="form-control" type="number" name="contact_num" id="contact_num">
+                                    <label for="contact_num">Contact Number <span class="text-danger fw-bold">*</span></label>
+                                    <div class="input-group ">
+                                   
+                                    <span class="input-group-text" id="inputGroupPrepend" style="background-color: #ffffff">+63</span>
+                                    <input class="form-control" type="number" name="contact_num" id="contact_num" aria-describedby="inputGroupPrepend"  pattern="9[0-9]{10}" title="Please enter 10 digits starting with '9'."  value="{{old('contact_num')}}" required>
+                                    @error('contact_num')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                     @enderror
+                                    </div>
                                 </div>
 
 
                                 <div class="col-12">
-                                    <label for="date_of_birth">Address</label>
-                                    <input class="form-control" type="text" name="address" id="address">
+                                    <label for="address">Address <span class="text-danger fw-bold">*</span></label>
+                                    <input class="form-control" value="{{old('address')}}" type="text" name="address" id="address">
+                                    @error('address')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 
                                 <h6 class="pt-3 fw-bold">
@@ -59,16 +97,30 @@
                                 </h6>
 
                                 <div class="col-4">
-                                    <label for="tin_num">Tin</label></label>
-                                    <input class="form-control" type="text" name="tin_num" id="tin_num">
+                                    <label for="tin_num">Tin <span class="text-danger fw-bold">*</span></label>
+                                    <input class="form-control" value="{{old('tin_num')}}" type="number" name="tin_num" id="tin_num">
+                                    @error('tin_num')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-4 ">
-                                    <label for="unit_id">Unit</label></label>
-                                    <input class="form-control" type="text" name="unit_id" id="contact_num">
+                                    <label for="unit_id">Unit <span class="text-danger fw-bold">*</span></label>
+                                    <select class="form-control form-select" name="unit_id" id="unit_id">
+                                        <option selected>ex. BUCS</option>
+                                        @foreach ($units as $unit)
+                                            <option value="{{$unit->id}}" {{old('unit_id') == $unit->id? 'selected' : ''  }}>{{$unit->unit_code}}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('unit_id')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-4 ">
-                                    <label for="position">Position</label></label>
-                                    <input class="form-control" type="number" name="position" id="position">
+                                    <label for="position">Position</label>
+                                    <input class="form-control" type="text" name="position" id="position" value="{{old('position')}}">
+                                    @error('position')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
 
                             </div>
@@ -77,36 +129,71 @@
                             <div class="row">
                             
                                 <div class="col-6">
-                                    <label for="monthly_salary">Monthly Salary</label></label>
-                                    <input class="form-control" type="number" name="monthly_salary" id="contact_num">
+                                    <label for="monthly_salary">Monthly Salary <span class="text-danger fw-bold">*</span></label>
+                                    <input class="form-control" type="number" name="monthly_salary" id="monthly_salary" value="{{old('monthly_salary')}}"> 
+                                    @error('monthly_salary')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-6">
-                                    <label for="employee_num">Employee No.</label></label>
-                                    <input class="form-control" type="number" name="employee_num" id="employee_num">
-                                </div>
-                                <div class="col-6">
-                                    <label for="bu_appointment_date">Date Appointed at BU</label></label>
-                                    <input class="form-control" type="date" name="bu_appointment_date" id="bu_appointment_date">
+                                    <label for="employee_num">Employee No.</label>
+                                    <input class="form-control" type="number" name="employee_num" id="employee_num" value="{{old('employee_num')}}">
+                                    @error('employee_num')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
 
+
+                                <div class="col-6">
+                                    <label for="bu_appointment_date">Date Appointed at BU</label>
+                                    <input class="form-control" type="date" name="bu_appointment_date" id="bu_appointment_date" value="{{old('bu_appointment_date')}}">
+                                    @error('bu_appointment_date')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
+                                </div>
+                                <div class="col-6">
+                                    <label  for="appointment_status">Status of Appointment</label>
+                                    <select name="appointment_status" class="form-select form-control validate" aria-label="Default select example" value="{{old('appointment_status')}}">
+                                        <option value="casual">Casual</option>
+                                        <option value="permanent">Permanent</option>
+                                    </select>
+                                    @error('appointment_statu')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
+                                </div>
+
+                                
+
                                 <h6 class="pt-3 fw-bold">
-                                    User Account Credentials
+                                    User Account Credentials <span class="text-danger fw-bold" style="font-size: 12px">All fields here required.</span>
                                 </h6>
                                 <div class="col-12">
-                                    <label for="email">Email</label></label>
-                                    <input class="form-control" type="email" name="email" id="email">
+                                    <label for="email">BU. Email</label>
+                                    <input class="form-control" type="email" name="email" id="email" value="{{old('email')}}">
+                                    @error('email')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-12">
-                                    <label for="passowrd">Password</label></label>
-                                    <input class="form-control" type="passowrd" name="passowrd" id="passowrd">
+                                    <label for="password">Password</label>
+                                    <input class="form-control" type="password" name="password" id="password">
+                                    @error('password')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                                 <div class="col-12">
-                                    <label for="password_confirmation">Confirm Password</label></label>
-                                    <input class="form-control" type="password_confirmation" name="password_confirmation" id="password_confirmation">
+                                    <label for="password_confirmation">Confirm Password</label>
+                                    <input class="form-control" type="password" name="password_confirmation" id="password_confirmation">
+                                    @error('password_confirmation')
+                                        <h6 style="font-size: 14px" class="text-danger pt-1"><i class="bi bi-exclamation-circle"></i> {{$message}}</h6>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
                         <div class="col-12 my-4 text-end">
+                            <p style="font-size: 12px" class="text-secondary text-start">
+                                By clicking Create, this member account created agrees to the terms and conditions of Bicol University Provident Fund and online terms of use of eBUPF website.
+                            </p>
                             <a class="btn btn-outline-secondary rounded-3" style="font-size: 14px" href="">
                                 Back
                             </a>
@@ -123,6 +210,18 @@
     
     </div>
 </div>
+<script>
+    const numericInput = document.getElementById('contact_num');
+  
+    numericInput.addEventListener('input', function () {
+      const value = numericInput.value;
+      if (!/9[0-9]{0,10}/.test(value) || value.length != 10 || value[0] != 9) {
+        numericInput.setCustomValidity('Please enter valid PH sim number format');
+      } else {
+        numericInput.setCustomValidity('');
+      }
+    });
+  </script>
 <script>
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
